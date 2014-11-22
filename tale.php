4839e43eae51ddf82d1e20484775d3da34253f55
s198517 <simenarvnes@gmail.com>
2014-11-22 16:29:50 +0100

    Talefunksjon
    Ny versjon av talefunksjon

--- tale.php
+++ tale.php
@@ -1,5 +1,32 @@
+<script>
+    
+    $(function() {  
+    $('#taleknapp').click(function(){
+    
+    var url = "?page=4";    
+    var $this = $(this);
 
+    if($this.hasClass('taleknapp')){
+        $this.text('Tale');
+    }
+    else{
+        //forandrer tekst og bakgrunn på knapp
+        $this.text('Opptak');
+        $this.css('background', 'red');
+        
+        //forandrer instruksjoner i bobbelen
+        $('.bubble').text("Trykk på 'Opptak'- knappen for å avslute."); 
+        
+        //sender brukeren til neste siden
+        $('#taleknapp').click(function(){
+           $(location).attr('href',url); 
+        });
+    }
+  });
+});
+
+</script>
 <div class="bubble">Trykk på "Tale"- knappen og forklar hvordan du føler deg.</div>
 
-<div id="taleknapp" class="knapp"><a href="?page=4" >Tale</a></div>
+<div id="taleknapp" class="knapp" class="talefunksjon">Tale</div>
 
